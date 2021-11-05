1e21687e065b817eda9480e4f24c731991b46097
gilad517 <88197808+gilad517@users.noreply.github.com>
2021-11-05 14:22:26 +0200

    Update server.py

--- Part_B/server.py
+++ Part_B/server.py
@@ -9,13 +9,14 @@ flag_got_max = False
 max_value = 0
 while True:
     data, address = s.recvfrom(100)
-    data = data.split(b':', 1)
-    if data[0].startswith(b"max"):
+    data = data.decode()
+    data = data.split(':', 1)
+    if data[0].startswith("max"):
         data[0] = data[0][3:]
         max_value = int(data[0])
         flag_got_max = True
     got[int(data[0])] = data[1]
-    s.sendto(b':'.join(data), address)
+    s.sendto(':'.join(data).encode(), address)
     if flag_got_max:
         got_all = True
         for index in range(1, max_value + 1):
@@ -28,4 +29,3 @@ while True:
             got = {}
             flag_got_max = False
             max_value = 0
-
