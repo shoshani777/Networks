427299d311ae20ac7bb788ff1451f4d465980090
gilad517 <88197808+gilad517@users.noreply.github.com>
2021-12-07 01:51:19 +0200

    Update client.py

--- client.py
+++ client.py
@@ -3,30 +3,25 @@ import utils
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 import sys
-import os
 import time
 
 
 def on_created(event):
-    print("on_created")
-    utils.send_file(event.src_path, server_sock)
+    log.extend(utils.send_file_deep(event.src_path, BASE_PATH))
 
 
 def on_deleted(event):
-    print("on_deleted")
-    utils.send_delete_file(event.src_path, server_sock)
+    log.append(utils.send_delete_file(event.src_path))
 
 
 def on_modified(event):
-    print("on_modified")
-    utils.send_delete_file(event.src_path, server_sock)
-    utils.send_file(event.src_path, server_sock)
+    log.append(utils.send_delete_file(event.src_path))
+    log.extend(utils.send_file_deep(event.src_path, BASE_PATH))
 
 
 def on_moved(event):
-    print("on_moved")
-    utils.send_delete_file(event.src_path, server_sock)
-    utils.send_file(event.dest_path, server_sock)
+    log.append(utils.send_delete_file(event.src_path))
+    log.extend(utils.send_file_deep(event.des_path, BASE_PATH))
 
 
 SERVER_IP = sys.argv[1]
@@ -34,21 +29,24 @@ SERVER_PORT = int(sys.argv[2])+utils.getnum()
 FOLDER_PATH = sys.argv[3]
 BASE_PATH = utils.norming_path("\\").join(FOLDER_PATH.split(utils.norming_path("\\"))[:-1])
 UPDATING_FREQUENCY = int(sys.argv[4])
-server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_sock.connect((SERVER_IP, SERVER_PORT))
+log = []
+personal_id = utils.make_ID()
+
 
 def main():
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_sock.connect((SERVER_IP, SERVER_PORT))
     if len(sys.argv) > 5:  # already connected
-        client_id = sys.argv[5]
-        server_sock.send(("ID " + client_id.decode()).encode())  # send id to server
-        utils.execute_operation(FOLDER_PATH, server_sock)  # get the updates from the server
+        ID = sys.argv[5]
+        server_sock.send(("ID " + personal_id + " " + ID).decode().encode())
+        utils.execute_log(BASE_PATH, server_sock)  # get the updates from the server
     else:
-        server_sock.send("give id".encode())  # send id (to get one) and the path to track
-        client_id = server_sock.recv(4096).decode()
-        print("client recieved id: " +client_id)
-        #time.sleep(1000)
-        utils.send_file_deep(FOLDER_PATH, server_sock, BASE_PATH)
-    time.sleep(1000)
+        server_sock.send(("give_id " + personal_id).encode())
+        ID = server_sock.recv(4096).decode()  # send current file
+        log.extend(utils.send_file_deep("", BASE_PATH))
+        utils.send_log(log, server_sock)
+        log.clear()
+    server_sock.close()
     patterns = ["*"]
     ignore_patterns = None
     ignore_directories = False
@@ -63,16 +61,15 @@ def main():
     my_observer = Observer()
     my_observer.schedule(my_event_handler, FOLDER_PATH, recursive=go_recursively)
     my_observer.start()
-    # try:
-    print("starting whiloop")
     while True:
         time.sleep(UPDATING_FREQUENCY)
-        server_sock.send((("update " + client_id).encode()))
-        utils.execute_operation(FOLDER_PATH, server_sock)  # get the updates from the server
-    # except:
-    #     my_observer.stop()
-    #     print("exeption")
-    my_observer.join()
+        server_sock.connect((SERVER_IP, SERVER_PORT))
+        server_sock.send(("update " + personal_id + " " + ID).encode())
+        utils.execute_log(BASE_PATH, server_sock)
+        utils.send_log(log, server_sock)
+        server_sock.close()
+        log.clear()
+
 
 if __name__ == '__main__':
     main()
