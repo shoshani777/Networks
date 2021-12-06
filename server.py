31b79e5282a777963a37731d1b853f3b0279de5d
shoshani777 <ori0586789889@gmail.com>
2021-12-07 01:49:48 +0200

    server done by the new method, cleared and cleaned.

--- server.py
+++ server.py
@@ -11,13 +11,6 @@ clientID_to_actionLog = dict()
 BASE_PATH = utils.norming_path(".\\clients")
 
 
-# #updating a client
-# def update(sock, ID, client_IP,client_port):
-#     for action in IP_and_port_to_actions_nissing[(client_IP,client_port)]:
-#         sock.send(action.encode())
-#     IP_and_port_to_actions_nissing[(client_IP, client_port)] = []
-
-
 def main():
     if(not os.path.isdir(BASE_PATH)):
         os.mkdir(BASE_PATH)
@@ -26,13 +19,6 @@ def main():
     server.listen(5)
     while True:
         client_socket, client_address = server.accept()
-
-        # if(client_address in IP_and_port_to_ID.keys()):
-        #     ID = IP_and_port_to_ID[client_address]
-        #     result_str = utils.execute_operation(utils.norming_path(BASE_PATH+"\\"+ID), client_socket)
-        # else:
-        #     result_str = utils.execute_operation(BASE_PATH, client_socket)
-
         msg = client_socket.recv(4096).decode()
         action = msg.split(" ")[0]
         clientID = msg.split(" ")[1]
@@ -42,70 +28,29 @@ def main():
             ID = utils.make_ID()
             ID_to_clientIDs[ID] = [].append(clientID)
             clientID_to_actionLog[clientID] = []
+            client_socket.send(ID)
             os.mkdir(utils.norming_path(BASE_PATH + "\\" + ID))
         elif (action == "ID"):
             was_ID = True
             ID = msg.split(' ')[2]
             ID_to_clientIDs[ID].append(clientID)
             clientID_to_actionLog[clientID] = []
-            utils.send_log(utils.file_deep_to_log(arguments))
-
-            # IP_and_port_to_ID[client_address] = ID
-            # if (len(os.listdir(os.path.normpath(BASE_PATH + "\\" + ID))) > 0):
-            #     for file in os.listdir(os.path.normpath(BASE_PATH + "\\" + ID)):
-            #         all_path = os.path.normpath(BASE_PATH + "\\" + ID + "\\" + file)
-            #         utils.send_file_deep(all_path, client_socket, os.path.normpath(BASE_PATH + "\\" + ID))
-            #     IP_and_port_to_actions_nissing[client_address] = []
-            # client_socket.send("#endoffunctions#".encode())
-
+            if(len(os.listdir(utils.norming_path(BASE_PATH + "\\" + ID)))==0):
+                utils.send_log([])
+            else:
+                for file in os.listdir(utils.norming_path(BASE_PATH + "\\" + ID)):
+                    utils.send_log(utils.send_file_deep(file,utils.norming_path(BASE_PATH + "\\" + ID)),client_socket)
         elif(action == "update"):
+            ID = msg.split(' ')[2]
             was_update = True
-            utils.send_log(clientID_to_actionLog[clientID],arguments)
-
-
+            utils.send_log(clientID_to_actionLog[clientID],clientID)
         if(not was_ID):
-            log = utils.execute_log(arguments)
+            log = utils.execute_log(utils.norming_path(BASE_PATH + "\\" + ID),client_socket)
             if(was_update):
                 for act in log:
-                    for key in ID_to_clientIDs.keys():
-                        if(clientID in ID_to_clientIDs[key]):
-                            right_ID = key
-                    for curr_clientID in ID_to_clientIDs[right_ID]:
+                    for curr_clientID in ID_to_clientIDs[ID]:
                         if(curr_clientID!=clientID):
                             clientID_to_actionLog[curr_clientID].append(act)
-
-
-
-
-
-
-        for string in result_str:
-            if (string.startswith("update")):
-                update(client_socket, IP_and_port_to_ID[client_address],client_address[0], client_address[1])
-                client_socket.send("#endoffunctions#".encode())
-                break
-            elif (string.startswith("give_id")):
-                ID = utils.make_ID()
-                IP_and_port_to_ID[client_address] = ID
-                IP_and_port_to_actions_nissing[client_address] = []
-                client_socket.send(IP_and_port_to_ID[client_address].encode())
-                os.mkdir(utils.norming_path(BASE_PATH+"\\"+ID))
-                break
-            elif (string.startswith("ID")):
-                ID = string.split(' ')[1]
-                IP_and_port_to_ID[client_address] = ID
-                if (len(os.listdir(os.path.normpath(BASE_PATH+"\\"+ID)))>0):
-                    for file in os.listdir(os.path.normpath(BASE_PATH+"\\"+ID)):
-                        all_path = os.path.normpath(BASE_PATH+"\\"+ID+"\\"+file)
-                        utils.send_file_deep(all_path,client_socket,os.path.normpath(BASE_PATH+"\\"+ID))
-                    IP_and_port_to_actions_nissing[client_address] = []
-                client_socket.send("#endoffunctions#".encode())
-                break
-            elif(string != ""):
-                ID = IP_and_port_to_ID[client_address]
-                for address in IP_and_port_to_actions_nissing.keys():
-                    if(IP_and_port_to_ID[address]!=ID and address!=client_address):
-                        IP_and_port_to_actions_nissing[client_address].append(string)
         client_socket.close()
 
 if __name__ == '__main__':
